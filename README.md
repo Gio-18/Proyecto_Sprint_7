2241aaa348d75dac57af306e8d0573b277684e24
Carlos Garcia <you@example.com>
2025-08-17 17:49:19 -0600

    Changes made on 18/08/2025

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Proyecto_Sprint_7
 Este es el proyecto para el sprint 7
+
+Este proyecto tiene como base información de ventas de autos de Estados Unidos.
+Podrás encontrar un histrograma de las ventas y una gráfica de disperción de la misma.
---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv('C:\Users\carlo\Documents\Tripleten\Modulo 7\Proyecto_Sprint_7\vehicles_us.csv')
+
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir gráfico de disperción')
+
+if disp_button:
+    st.write('Creación de una gráfica de disperción para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.scatter(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
